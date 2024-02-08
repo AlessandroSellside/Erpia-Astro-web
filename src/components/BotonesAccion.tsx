151950ca3180be1d7bd3b891c621b1323180b925
AlessandroSellside <Alessandro.Diaz@sellside.cl>
2024-02-08 12:19:23 -0600

    inicializacion de astro

---
+++ src/components/BotonesAccion.tsx
@@ -0,0 +1,10 @@
+ //imports state/effect/funciones
+ 
+ const BotonesAccion = () => {
+    //logica de subida de archivos
+
+  return (
+    <div>BotonesAccion</div>
+  )
+}
+export default BotonesAccion;
---
+++ src/components/SubirArchivo.tsx
@@ -0,0 +1,10 @@
+//imports state/effect/ funcionalidad
+const SubirArchivo = () => {
+    //Logica y consumo de Cloud function
+
+  return (
+    <div>SubirArchivo</div>
+  )
+}
+
+export default SubirArchivo;
